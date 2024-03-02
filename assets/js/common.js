05c330e7104873e0db622135ea5ba756dd7cdb72
emreyilmaz00 <96991836+emreyilmaz00@users.noreply.github.com>
2024-03-02 21:16:34 +0100

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,3 @@
-// add toggle functionality to abstract and bibtex buttons
 $(document).ready(function() {
   // add toggle functionality to abstract and bibtex buttons
   $('a.abstract').click(function() {
@@ -9,7 +8,17 @@ $(document).ready(function() {
     $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
   });
-  }
+  $('a').removeClass('waves-effect waves-light');
+
+  // bootstrap-toc
+  if($('#toc-sidebar').length){
+    var navSelector = "#toc-sidebar";
+    var $myNav = $(navSelector);
+    Toc.init($myNav);
+    $("body").scrollspy({
+      target: navSelector,
+  });
+}
 
   // add css to jupyter notebooks
   const cssLink = document.createElement("link");
@@ -27,24 +36,12 @@ $(document).ready(function() {
 
   $('.jupyter-notebook-iframe-container iframe').each(function() {
     $(this).contents().find("head").append(cssLink);
-      
-  // bootstrap-toc
-  if($('#toc-sidebar').length){
-    var navSelector = "#toc-sidebar";
-    var $myNav = $(navSelector);
-    Toc.init($myNav);
-    $("body").scrollspy({
-      target: navSelector,
-    });
-    $('a').removeClass('waves-effect waves-light');
-});
-
     if (theme == "dark") {
       $(this).bind("load",function(){
         $(this).contents().find("body").attr({
           "data-jp-theme-light": "false",
           "data-jp-theme-name": "JupyterLab Dark"});
-     });
+      });
     }
   });
 });
