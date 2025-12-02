f69b94b40f3d2eb09ae828529347fb9fbec55ad2
emreyilmaz00 <96991836+emreyilmaz00@users.noreply.github.com>
2025-12-02 01:09:04 +0000

    Deploying to gh-pages from @ emreyilmaz00/emreyilmaz00.github.io@0d5c151fd89774a4115b529dee5f168b5d64a729 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,9 +113,9 @@ ninja.data = [{
           description: "Multi Agent Path Planning, MPC, Deep RL, MuZero, Pseudospectral Methods",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-system-reliability",
+          title: 'System Reliability',
+          description: "Fault Tree Analysis, The Propagation of Statistical Moments, Resource Allocation, Augmented Lagrangian",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
